2bb5c8b485790e9f5654c36c5f043a4d4f8a0b4b
rotiple <opveopve@gmail.com>
2024-11-05 02:03:28 +0900

    3 heart loading

---
+++ inc/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('copy', (event) => {
+    event.preventDefault();
+});
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        document.getElementById('animationContainer').style.display = 'none';
+        document.body.style.backgroundColor = '#ffffff'; // 청첩장 페이지 배경을 흰색으로 변경
+        document.getElementById('invitationContent').style.display = 'block';
+    }, 3000);
+});
+
+
+
