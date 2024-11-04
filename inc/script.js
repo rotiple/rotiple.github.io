d4ef0e759b866ebd0d9524fca839a3621e8c04d4
rotiple <opveopve@gmail.com>
2024-11-05 02:48:17 +0900

    wedding font

--- inc/script.js
+++ inc/script.js
@@ -7,6 +7,6 @@ window.addEventListener('load', () => {
         document.getElementById('animationContainer').style.display = 'none';
         document.body.style.backgroundColor = '#ffffff'; // 청첩장 페이지 배경을 흰색으로 변경
         document.getElementById('invitationContent').style.display = 'block';
-    }, 6000);
+    }, 4000);
 });
 
