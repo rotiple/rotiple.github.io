b019e883d82372224bf8066da9fb9297145120ea
rotiple <opveopve@gmail.com>
2024-11-05 13:38:56 +0900

    calendar

--- inc/script.js
+++ inc/script.js
@@ -2,11 +2,11 @@ document.addEventListener('copy', (event) => {
     event.preventDefault();
 });
 
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         document.getElementById('animationContainer').style.display = 'none';
         document.body.style.backgroundColor = '#ffffff'; // 청첩장 페이지 배경을 흰색으로 변경
         document.getElementById('invitationContent').style.display = 'block';
-    }, 4000);
+    }, 3500);
 });
 
